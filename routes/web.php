82a261b7fb5374784d8b9db4ccb868df5fb2191b
Wilson <wcastropaixao@gmail.com>
2021-04-13 15:03:42 -0300

    Adiciona a rota e a classe de Controller do Produto

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('produto', ProductController::class);
 
 Auth::routes();
 
