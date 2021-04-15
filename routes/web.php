90e46bc8f2d49e3f391c9aad23f428da9f6add20
Wilson <wcastropaixao@gmail.com>
2021-04-15 15:33:27 -0300

    Adiciona a rota e a classe de Controller do Fabricante

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\ManufacturerController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,6 +14,7 @@ Route::get('/', function () {
 Route::resource('produto', ProductController::class);
 Route::resource('categorias', CategoryController::class);
 Route::resource('categorias-produtos', ProductCategoryController::class);
+Route::resource('fabricantes', ManufacturerController::class);
 
 Auth::routes();
 
