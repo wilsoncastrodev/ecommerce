3f3baac06ce40a47274828a50877f9b0af291b67
Wilson <wcastropaixao@gmail.com>
2021-04-16 18:29:15 -0300

    Implementa o método "store" do Cadastro de Categorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -36,7 +37,23 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'category_title' => 'required|unique:categories|max:255',
+            'category_top' => 'required',
+            'category_image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048'
+        ]);
+
+        $image = $request->file('category_image');
+        $file_name = Str::slug($request->category_title) . '_' . time() . '.' . $image->getClientOriginalExtension();
+        $path = $image->storeAs('public/imagens/categorias', $file_name);
+
+        $category = new Category;
+        $category->category_title = $request->category_title;
+        $category->category_top = $request->category_top;
+        $category->category_image = $path;
+        $category->save();
+        
+        return redirect()->back()->with('success', 'Categoria cadastrada com sucesso!');
     }
 
     /**
